ee0f55a64fee5ace0ef272f018ef45f7f73fbe4b
unknown <sebbbes2@gmail.com>
2023-01-17 18:22:27 +0100

    canvas blir de :()

--- interface/src/todo-list.tsx
+++ interface/src/todo-list.tsx
@@ -1,24 +1,22 @@
-import { For, createSignal } from 'solid-js';
+import { randomInt } from 'mathjs';
+import { For, createSignal, createEffect } from 'solid-js';
 
 type Point = { x: number; y: number};
 
 export const PointList = () => {
-  let input!: HTMLInputElement;
+  let canvas:HTMLCanvasElement|undefined = undefined;
   const ylim = [-250, 250]
   const speed = 1 // * px per 10ms
   const initPoints: Point[] = []
-  for (let x = 0; x < 500; x += 500/1000) {
-    let y = 250*Math.sin(2*Math.PI* x/250) - ylim[0]
+  for (let x = 0; x < 500; x += 500/40000) {
+    let y = randomInt(0,ylim[1]-ylim[0])//250*Math.sin(2*Math.PI* x/250) - ylim[0]
     initPoints.push({x:x,y:y})
   } 
   const startT = Date.now()
   const [points, setPoints] = createSignal<Point[]>(initPoints);
   const [t, setT] = createSignal<number>(0);
   
-
-  let oldx:number, oldy:number, x:number, y:number = NaN
-  
-  const update = () => {
+  const updateT = () => {
     const nPoints: Point[] = []
     for (const p of points()) {
       const nx = p.x
@@ -30,35 +28,32 @@ export const PointList = () => {
     setT(Math.round((Date.now()-startT)/1000))
   }
 
-  setInterval(update,10)
+  const updateCtx = (canvas:HTMLCanvasElement|undefined, points:Point[]) => {
+    if (canvas === undefined) return 0
+    const ctx = canvas.getContext("2d")
+    if (ctx === null) return 0
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath()
+    let first = true
+    for (const p of points) {
+      if (first) {
+        ctx.moveTo(p.x,p.y)
+        first = false
+        continue
+      }
+      ctx.lineTo(p.x, p.y)
+    }
+    ctx.stroke()
+  }
+
+
+  setInterval(updateT,10)
+  createEffect(() => updateCtx(canvas, points()))
 
   return (
     <>
-    <button onClick={update} >jump 1s</button>
     <div>{t}</div>
-    <svg width={500} height={ylim[1]-ylim[0]}>
-      <For each={points()}>
-        {(point) => {
-          x = point.x;
-          y = point.y;
-          
-
-          if (!isNaN(oldx) && !isNaN(oldy)) {
-            const sx = x.toString()
-            const sy = y.toString()
-            const soldx = oldx.toString()
-            const soldy = oldy.toString()
-            oldx = x;
-            oldy = y;
-            return (
-              <line x1={soldx} y1={soldy} x2={sx} y2={sy} stroke="black" />
-            );
-          }
-          oldx = x;
-          oldy = y;
-        }}
-      </For>
-    </svg>
+    <canvas ref={canvas} width={500} height={500} />
     </>
   );
 };
